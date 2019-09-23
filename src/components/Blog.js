cbc1f01265506095a7a2b099067d4fbe34078922
Marco Richetta <marcorichetta@gmail.com>
2019-09-23 14:52:14 -0300

    Now Blog posts can be expanded to show more information
    - Like button still does nothing
    - Added a bit of CSS

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    padding: 7,
+    border: 'solid',
+    borderColor: 'cornflowerblue',
+    borderWidth: 4,
+    borderRadius: 10,
+    marginBottom: 5
+  }
+
+  const [expanded, setExpanded] = useState(false)
+
+  const showExpanded = () => {
+    setExpanded(!expanded)
+    console.log('Expanded')
+  }
+
+  return (
+    <div style={blogStyle} onClick={showExpanded}>
+      {expanded === false ?
+        <div>
+          {blog.title} - {blog.author}
+        </div>
+        :
+        <div onClick={showExpanded}>
+          <ul>
+            <li>{blog.title}</li>
+            <li>{blog.url === "" ? "No url provided" : blog.url}</li>
+            <li>{blog.likes} <button onClick={() => console.log('liked')}>Like</button></li>
+            <li>Added by {blog.author}</li>
+          </ul>
+ 
+        </div>}
+    </div>
+  )
+}
 
 export default Blog
